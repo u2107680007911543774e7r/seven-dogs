7196ad0df11de0eb8566d414058ce7f062fad1c3
saadow <vadim.chumasov@gmail.com>
2021-08-19 19:22:49 +0300

    loading feature v1

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def seven_dogs():
     keyword = input("Input your keyword：", type=TEXT)
 
     start = time.time()
-    with put_loading():
+    with put_text('Awaiting results...', position='center'), put_loading(shape='grow', color='dark', position='center'):
         content = generate_table_content(keyword)
         put_markdown(f'# **Search results \"{keyword}\":**')
         put_table(content, header=['Title', 'Price', 'Shop', 'Link'])
